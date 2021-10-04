20ea79914be7d36517180a89fd610252df374e35
cesarpasillas <cesar.epasillas@gmail.com>
2021-10-04 17:38:49 -0500

    class with the fields and methods

---
+++ src/com/programming/squirrell/tutorial/lesson1/Squirrel.java
@@ -0,0 +1,20 @@
+package com.programming.squirrell.tutorial.lesson1;
+
+/**
+ * This class called Squirrel, and is the simplest class that you will found.
+ */
+public class Squirrel {
+
+    //This is a field of the Squirrel Class
+    String name;
+
+    //this is a method of the Squirrel Class
+    public String getName() {
+        return name;
+    }
+
+    //this is a method of the Squirrel Class
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/com/programming/squirrell/tutorial/lesson1/Squirrell.java
+++
@@ -1,8 +0,0 @@
-package com.programming.squirrell.tutorial.lesson1;
-
-/**
- * This class called Squirrell, and is the simplest class that you will found.
- */
-public class Squirrell {
-
-}

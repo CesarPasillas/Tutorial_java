c08420b764074e0ce5a5e9a0aea63a934099d4cd
cesarpasillas <cesar.epasillas@gmail.com>
2021-10-11 15:59:44 -0500

    Class vs Files, ejemplos de classes, add some comments

--- src/com/programming/squirrell/tutorial/lesson3/MyClass.java
+++ src/com/programming/squirrell/tutorial/lesson3/MyClass.java
@@ -1,8 +1,19 @@
 package com.programming.squirrell.tutorial.lesson3;
+//The MyClass File has two classes, LittleSquirrel and RedSquirrel
 
+/**
+ * Class LittleSquirrel
+ */
 class LittleSquirrel {
+
 }
 
+/**
+ * Class RedSquirrell
+ */
 class RedSquirrel {
 
 }
+
+//Is possible add more classes as are required
+

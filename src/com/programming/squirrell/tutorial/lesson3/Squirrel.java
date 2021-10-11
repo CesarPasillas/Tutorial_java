6f69f9fa2129a9b5ab86da0ac987df758b16c0b2
cesarpasillas <cesar.epasillas@gmail.com>
2021-10-11 16:01:57 -0500

    Class vs Files, ejemplos de classes, add some comments

--- src/com/programming/squirrell/tutorial/lesson3/Squirrel.java
+++ src/com/programming/squirrell/tutorial/lesson3/Squirrel.java
@@ -1,8 +1,15 @@
 package com.programming.squirrell.tutorial.lesson3;
 
+/**
+ * The public class Squirrel, the *.java file must have the name of this class because like:
+ *   Squirrel.java
+ */
 public class Squirrel {
 }
 
+/**
+ * THis is anotehr class called Squirrel2
+ */
 class Squirrel2 {
 
 }

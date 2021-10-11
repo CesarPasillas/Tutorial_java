1283efc418fd02a606c332d8d7257e0b14b05479
cesarpasillas <cesar.epasillas@gmail.com>
2021-10-11 14:05:55 -0500

    Class vs Files, ejemplos de classes

---
+++ src/com/programming/squirrell/tutorial/lesson3/MyClass.java
@@ -0,0 +1,8 @@
+package com.programming.squirrell.tutorial.lesson3;
+
+class LittleSquirrel {
+}
+
+class RedSquirrel {
+
+}
---
+++ src/com/programming/squirrell/tutorial/lesson3/Squirrel.java
@@ -0,0 +1,8 @@
+package com.programming.squirrell.tutorial.lesson3;
+
+public class Squirrel {
+}
+
+class Squirrel2 {
+
+}
